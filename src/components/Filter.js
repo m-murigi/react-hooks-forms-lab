49944e1a11af96c8b54a4c53a0d7425b60d09097
Emmanuel <zacmac807@gmail.com>
2024-05-04 11:54:03 +0300

    add file

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,69 @@
+// import React from "react";
+
+// function Filter({ onCategoryChange }) {
+//   return (
+//     <div className="Filter">
+//       <input type="text" name="search" placeholder="Search..." />
+//       <select name="filter" onChange={onCategoryChange}>
+//         <option value="All">Filter by category</option>
+//         <option value="Produce">Produce</option>
+//         <option value="Dairy">Dairy</option>
+//         <option value="Dessert">Dessert</option>
+//       </select>
+//     </div>
+//   );
+// }
+
+// export default Filter;
+
+
+
+// import React, { useState } from "react";
+
+// function Filter({ onCategoryChange, onSearchChange }) {
+//   const [searchText, setSearchText] = useState("");
+
+//   function handleSearchChange(event) {
+//     const text = event.target.value;
+//     setSearchText(text);
+//     onSearchChange(text); // Pass the search text to the parent component
+//   }
+
+//   return (
+//     <div className="Filter">
+//       <input 
+//         type="text" 
+//         name="search" 
+//         placeholder="Search..." 
+//         value={searchText} // Connect the input field to state
+//         onChange={handleSearchChange} // Handle input change
+//       />
+//       <select name="filter" onChange={onCategoryChange}>
+//         <option value="All">Filter by category</option>
+//         <option value="Produce">Produce</option>
+//         <option value="Dairy">Dairy</option>
+//         <option value="Dessert">Dessert</option>
+//       </select>
+//     </div>
+//   );
+// }
+
+// export default Filter;
+
+
+
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange, search }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        value={search}
+        onChange={onSearchChange}
+        type="text"
+        name="search"
+        placeholder="Search..."
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
